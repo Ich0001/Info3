c32629034cce888d1d42d2e882ae0a6f98b0d097
Ich0001 <JPfrommer@freenet.de>
2017-06-30 20:35:22 +0200

    Noch uebrige Datei KonfigurationsFensterView dazu
    Noch uebrige Datei KonfigurationsFensterView dazu geladen. Die hat es
    beim vorgherigen Commit nicht angezeigt.

--- src/view/KonfigurationsfensterView.java
+++ src/view/KonfigurationsfensterView.java
@@ -19,10 +19,11 @@ public class KonfigurationsFensterView extends JPanel {
 	private KonfigurationView konfiguration;
 	private JButton buttonSave;
 	private JPanel footer;
+	KonfigurationView konfigurationsFensterIdle = new KonfigurationViewIdle();
 
 	public KonfigurationsFensterView() {
-		konfiguration = new KonfigurationViewPause(ControlModel.getInstance());
-		konfiguration.setVisible(false);
+		konfiguration = konfigurationsFensterIdle;
+
 		buttonSave = new JButton("Save");
 		footer = new JPanel();
 
@@ -32,177 +33,191 @@ public class KonfigurationsFensterView extends JPanel {
 
 			@Override
 			public void actionPerformed(ActionEvent arg0) {
+				try {
+					// Direction
+					if (konfiguration instanceof KonfigurationViewDirection) {
+						String degree = ((KonfigurationViewDirection) konfiguration)
+								.getTextDegree();
+						konfiguration.clearInput();
+						int selRow = ControlDeveloperView.getInstance()
+								.getOurJTable().getSelectedRow();
+						Command command = ControlModel.getInstance()
+								.getControlProcess().get(selRow);
+						// TODO exception-handling fuer den Fall dass nicht nur
+						// Zahlen eingegeben wurden
+						((Direction) command).setDegree(Integer
+								.parseInt(degree));
 
-				// Direktion
-				if (konfiguration instanceof KonfigurationViewDirection) {
-					String degree = ((KonfigurationViewDirection) konfiguration)
-							.getTextDegree();
-					konfiguration.clearInput();
-					int selRow = ControlDeveloperView.getInstance()
-							.getOurJTable().getSelectedRow();
-					Command command = ControlModel.getInstance()
-							.getControlProcess().get(selRow);
-					// TODO exception-handling fuer den Fall dass nicht nur
-					// Zahlen eingegeben wurden
-					((Direction) command).setDegree(Integer.parseInt(degree));
+						// System.out.println("Degree="
+						// + ((Direction) (ControlModel.getInstance()
+						// .getControlProcess().get(selRow)))
+						// .getDegree());
+						// ControlDeveloperView
+						// .getInstance()
+						// .getTextArea()
+						// .append("\n>>Zeile "
+						// + (selRow + 1)
+						// + ": Degree="
+						// + ((Direction) (ControlModel
+						// .getInstance()
+						// .getControlProcess()
+						// .get(selRow))).getDegree());
 
-					System.out.println("Degree="
-							+ ((Direction) (ControlModel.getInstance()
-									.getControlProcess().get(selRow)))
-									.getDegree());
+					}
+
+					// Gear
+					else if (konfiguration instanceof KonfigurationViewGear) {
+						String speed = ((KonfigurationViewGear) konfiguration)
+								.getTextSpeed();
+						String duration = ((KonfigurationViewGear) konfiguration)
+								.getTextDuration();
+						konfiguration.clearInput();
+
+						int selRow = ControlDeveloperView.getInstance()
+								.getOurJTable().getSelectedRow();
+						Command command = ControlModel.getInstance()
+								.getControlProcess().get(selRow);
+						// TODO exception-handling fuer den Fall dass nicht nur
+						// Zahlen eingegeben wurden
+						((Gear) command).setSpeed(Integer.parseInt(speed));
+						((Gear) command).setDuration(Double
+								.parseDouble(duration));
+
+						// System.out.println("speed="
+						// + ((Gear) (ControlModel.getInstance()
+						// .getControlProcess().get(selRow)))
+						// .getSpeed()
+						// + "  duration="
+						// + ((Gear) (ControlModel.getInstance()
+						// .getControlProcess().get(selRow)))
+						// .getDuration());
+						// System.out.println("speed:" + speed + "  duration:"
+						// + duration);
+						// ControlDeveloperView
+						// .getInstance()
+						// .getTextArea()
+						// .append("\n>>Zeile "
+						// + (selRow + 1)
+						// + ": Speed="
+						// + ((Gear) (ControlModel.getInstance()
+						// .getControlProcess()
+						// .get(selRow))).getSpeed()
+						// + "  Duration="
+						// + ((Gear) (ControlModel.getInstance()
+						// .getControlProcess()
+						// .get(selRow))).getDuration());
+					}
+
+					// Repetition
+					else if (konfiguration instanceof KonfigurationViewRepetition) {
+						String nrSteps = ((KonfigurationViewRepetition) konfiguration)
+								.getTextNrSteps();
+						String nrRepetitions = ((KonfigurationViewRepetition) konfiguration)
+								.getTextNrRepetitions();
+						konfiguration.clearInput();
+
+						int selRow = ControlDeveloperView.getInstance()
+								.getOurJTable().getSelectedRow();
+						Command command = ControlModel.getInstance()
+								.getControlProcess().get(selRow);
+						// TODO exception-handling fuer den Fall dass nicht nur
+						// Zahlen eingegeben wurden
+						((Repetition) command).setNrSteps(Integer
+								.parseInt(nrSteps));
+						((Repetition) command).setNrRepetitions(Integer
+								.parseInt(nrRepetitions));
+
+						// System.out.println("NrSteps="
+						// + ((Repetition) (ControlModel.getInstance()
+						// .getControlProcess().get(selRow)))
+						// .getNrSteps()
+						// + "  NrRepetitions="
+						// + ((Repetition) (ControlModel.getInstance()
+						// .getControlProcess().get(selRow)))
+						// .getNrRepetitions());
+						// System.out.println("NrSteps:" + nrSteps
+						// + "  NrRepetitions:" + nrRepetitions);
+						// ControlDeveloperView
+						// .getInstance()
+						// .getTextArea()
+						// .append("\n>>Zeile "
+						// + (selRow + 1)
+						// + ": NrSteps="
+						// + ((Repetition) (ControlModel
+						// .getInstance()
+						// .getControlProcess()
+						// .get(selRow))).getNrSteps()
+						// + "  NrRepetitions="
+						// + ((Repetition) (ControlModel
+						// .getInstance()
+						// .getControlProcess()
+						// .get(selRow)))
+						// .getNrRepetitions());
+					}
+
+					// Pause
+					else if (konfiguration instanceof KonfigurationViewPause) {
+						String duration = ((KonfigurationViewPause) konfiguration)
+								.getTextDuration();
+						konfiguration.clearInput();
+
+						int selRow = ControlDeveloperView.getInstance()
+								.getOurJTable().getSelectedRow();
+						Command command = ControlModel.getInstance()
+								.getControlProcess().get(selRow);
+						// TODO exception-handling fuer den Fall dass nicht nur
+						// Zahlen eingegeben wurden
+						((Pause) command).setDuration(Double
+								.parseDouble(duration));
+
+						// System.out.println("duration="
+						// + ((Pause) (ControlModel.getInstance()
+						// .getControlProcess().get(selRow)))
+						// .getDuration());
+						// System.out.println("duration:" + duration);
+						// ControlDeveloperView
+						// .getInstance()
+						// .getTextArea()
+						// .append("\n>>Zeile "
+						// + (selRow + 1)
+						// + ": Duration="
+						// + ((Pause) (ControlModel.getInstance()
+						// .getControlProcess()
+						// .get(selRow))).getDuration());
+
+					} else {
+						System.out
+								.println("Fehler in KonfigurationsFensterView :-(");
+					}
+
+					// Setze die geaenderten Inhalte aus der verketteten Liste
+					// in das TableModel um
 					ControlDeveloperView
 							.getInstance()
-							.getTextArea()
-							.append("\n>>Zeile "
-									+ (selRow + 1)
-									+ ": Degree="
-									+ ((Direction) (ControlModel.getInstance()
-											.getControlProcess().get(selRow)))
-											.getDegree());
+							.getOurJTable()
+							.getTableModel()
+							.setValueAt(
+									ControlModel
+											.getInstance()
+											.getControlProcess()
+											.get(ControlDeveloperView
+													.getInstance()
+													.getOurJTable()
+													.getSelectedRow())
+											.propertiesToString(),
+									ControlDeveloperView.getInstance()
+											.getOurJTable().getSelectedRow(), 2);
+				}
+
+				catch (NumberFormatException e) {
+					System.out.println("Scheisse gebaut");
+					ControlDeveloperView.getInstance().println(
+							">>Keine oder Falsche Zeichen eingegeben!");
+				} finally {
 
 				}
 
-				// Gear
-				else if (konfiguration instanceof KonfigurationViewGear) {
-					String speed = ((KonfigurationViewGear) konfiguration)
-							.getTextSpeed();
-					String duration = ((KonfigurationViewGear) konfiguration)
-							.getTextDuration();
-					konfiguration.clearInput();
-
-					int selRow = ControlDeveloperView.getInstance()
-							.getOurJTable().getSelectedRow();
-					Command command = ControlModel.getInstance()
-							.getControlProcess().get(selRow);
-					// TODO exception-handling fuer den Fall dass nicht nur
-					// Zahlen eingegeben wurden
-					((Gear) command).setSpeed(Integer.parseInt(speed));
-					((Gear) command).setDuration(Double.parseDouble(duration));
-
-					System.out.println("speed="
-							+ ((Gear) (ControlModel.getInstance()
-									.getControlProcess().get(selRow)))
-									.getSpeed()
-							+ "  duration="
-							+ ((Gear) (ControlModel.getInstance()
-									.getControlProcess().get(selRow)))
-									.getDuration());
-
-					ControlDeveloperView
-							.getInstance()
-							.getTextArea()
-							.append("\n>>Zeile "
-									+ (selRow + 1)
-									+ ": Speed="
-									+ ((Gear) (ControlModel.getInstance()
-											.getControlProcess().get(selRow)))
-											.getSpeed()
-									+ "  Duration="
-									+ ((Gear) (ControlModel.getInstance()
-											.getControlProcess().get(selRow)))
-											.getDuration());
-
-					System.out.println("speed:" + speed + "  duration:"
-							+ duration);
-				}
-
-				// Repetition
-				else if (konfiguration instanceof KonfigurationViewRepetition) {
-					String nrSteps = ((KonfigurationViewRepetition) konfiguration)
-							.getTextNrSteps();
-					String nrRepetitions = ((KonfigurationViewRepetition) konfiguration)
-							.getTextNrRepetitions();
-					konfiguration.clearInput();
-
-					int selRow = ControlDeveloperView.getInstance()
-							.getOurJTable().getSelectedRow();
-					Command command = ControlModel.getInstance()
-							.getControlProcess().get(selRow);
-					// TODO exception-handling fuer den Fall dass nicht nur
-					// Zahlen eingegeben wurden
-					((Repetition) command).setNrSteps(Integer.parseInt(nrSteps));
-					((Repetition) command).setNrRepetitions(Integer
-							.parseInt(nrRepetitions));
-
-					System.out.println("NrSteps="
-							+ ((Repetition) (ControlModel.getInstance()
-									.getControlProcess().get(selRow)))
-									.getNrSteps()
-							+ "  NrRepetitions="
-							+ ((Repetition) (ControlModel.getInstance()
-									.getControlProcess().get(selRow)))
-									.getNrRepetitions());
-
-					System.out.println("NrSteps:" + nrSteps
-							+ "  NrRepetitions:" + nrRepetitions);
-					ControlDeveloperView
-							.getInstance()
-							.getTextArea()
-							.append("\n>>Zeile "
-									+ (selRow + 1)
-									+ ": NrSteps="
-									+ ((Repetition) (ControlModel.getInstance()
-											.getControlProcess().get(selRow)))
-											.getNrSteps()
-									+ "  NrRepetitions="
-									+ ((Repetition) (ControlModel.getInstance()
-											.getControlProcess().get(selRow)))
-											.getNrRepetitions());
-				}
-
-				// Pause
-				else if (konfiguration instanceof KonfigurationViewPause) {
-					String duration = ((KonfigurationViewPause) konfiguration)
-							.getTextDuration();
-					konfiguration.clearInput();
-
-					int selRow = ControlDeveloperView.getInstance()
-							.getOurJTable().getSelectedRow();
-					Command command = ControlModel.getInstance()
-							.getControlProcess().get(selRow);
-					// TODO exception-handling fuer den Fall dass nicht nur
-					// Zahlen eingegeben wurden
-					((Pause) command).setDuration(Double.parseDouble(duration));
-
-					System.out.println("duration="
-							+ ((Pause) (ControlModel.getInstance()
-									.getControlProcess().get(selRow)))
-									.getDuration());
-
-					System.out.println("duration:" + duration);
-
-					ControlDeveloperView
-							.getInstance()
-							.getTextArea()
-							.append("\n>>Zeile "
-									+ (selRow + 1)
-									+ ": Duration="
-									+ ((Pause) (ControlModel.getInstance()
-											.getControlProcess().get(selRow)))
-											.getDuration());
-
-				} else
-					System.out
-							.println("Fehler in KonfigurationsFensterView :-(");
-				ControlDeveloperView
-						.getInstance()
-						.getOurJTable()
-						.getTableModel()
-						.setValueAt(
-								ControlModel
-										.getInstance()
-										.getControlProcess()
-										.get(ControlDeveloperView.getInstance()
-												.getOurJTable()
-												.getSelectedRow())
-										.propertiesToString(),
-								ControlDeveloperView.getInstance()
-										.getOurJTable().getSelectedRow(), 2);
-
-				;
-				System.out.println("fire");
 			}
-
 		});
 		footer.setLayout(new FlowLayout(FlowLayout.RIGHT));
 		footer.add(buttonSave);
@@ -213,6 +228,10 @@ public class KonfigurationsFensterView extends JPanel {
 
 	public KonfigurationView getKonfiguration() {
 		return konfiguration;
+	}
+
+	public void setKonfigurationIdle() {
+		changeKonfiguration(konfigurationsFensterIdle);
 	}
 
 	public void changeKonfiguration(KonfigurationView newKonfiguration) {
